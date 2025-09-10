b990ae0133e1c920560c130d3936105e9d16df80
Dace Liepina <dace.liepina77@gmail.com>
2025-09-10 17:53:49 +0200

    added Dog und Profile Cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
 import './App.css'
-import AgeInfo from './components/AgeInfo/AgeInfo'
-import Goodbye from './components/Goodbye/Goodbye'
+//import AgeInfo from './components/AgeInfo/AgeInfo'
+import DogCard from './components/DogCard/DogCard'
+//import Goodbye from './components/Goodbye/Goodbye'
 import Greeting from './components/Greeting/Greeting'
+import ProfileCard from './components/ProfileCard/ProfileCard'
 
 function App() {
   return (
     <div>
       <p>Hello, React!</p>
-      < Greeting />
-      < Goodbye name ={"Bob"}/>
+      < Greeting/>
+      < DogCard />
+      {/*< Goodbye name ={"Bob"}/>
       < Goodbye name={"John"}/>
       <AgeInfo age = {23} name={"Fred"}/>
       <AgeInfo age = {44} name={"Katja"}/>
-      <p>Live</p>
+      <p>Live</p>*/}
+      
+      <ProfileCard
+      avatar="/src/assets/images/my-image.png"
+        name="Vite React"
+        description="Vite ist ein modernes Build- und Frontend-Entwicklungswerkzeug, das ältere Lösungen wie Webpack ersetzt."
+        />
       
     </div>
   )
---
+++ src/components/DogCard/DogCard.tsx
@@ -0,0 +1,7 @@
+function DogCard (){
+    return <div>
+        <p>I love my dog</p>
+    </div>
+}
+
+export default DogCard;
